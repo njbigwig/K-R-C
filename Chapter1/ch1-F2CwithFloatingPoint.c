550f709172d8da6abc85db1a641f694f63d8bff0
Dave Dempski <59418732+njbigwig@users.noreply.github.com>
2023-05-08 15:37:50 -0700

    Added more Chapter 1 sample programs

--- Chapter1/ch1-F2CwithFloatingPoint.c
+++ Chapter1/ch1-F2CwithFloatingPoint.c
@@ -1,15 +1,15 @@
 /* Convert Fahrenheit to Celsius based on this formula:
 
-   C = (5/9)(F-32)
+   C = (5.0/9.0)(F-32.0)
 
-   This version uses integer variables, so 5/9 was modified to avoid multiplying by 0 due to integer truncation:
-     5/9 = 0
-     5%9 = 5  C Modus operator
+   printf Fahrenheit-Celsius table - floating-point version
+
+     for fahr, 20, ..., 300
 
     Changed main to int and added a return value to avoid compiler warnings
 
     compiled by:
-      bcc32c ch1-F2C.c [ENTER]
+      bcc32c ch1-F2CwithFloatingPoint.c [ENTER]
 
 */
 
@@ -21,7 +21,7 @@
 int main()
 {
 
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;      /* lower limit of temperature table */
@@ -31,10 +31,10 @@ int main()
     fahr = lower;
     while ( fahr <= upper )
     {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 
-    return (0);
+    return (0 );
 }
